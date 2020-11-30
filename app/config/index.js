703397e75dbcf8a08f53d5245548fdfe39ec5bf6
Maxim Kostin <maxim@moonshinetechnology.com>
2020-11-30 16:03:30 +0200

    feat: staging configuration

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,7 @@
 import { configDev } from './config.dev';
-import { configProd } from './config.prod';
+// import { configProd } from './config.prod';
+import { configStag } from './config.stag';
 
 const IS_DEV = true;
-const config = IS_DEV ? configDev : configProd;
+const config = IS_DEV ? configDev : configStag;
 export { config };
